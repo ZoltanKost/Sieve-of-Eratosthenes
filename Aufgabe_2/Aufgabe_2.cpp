f097f683dc8ac5d96b89627b6dcaaed12826f409
ZoltanKost <mikle.mikle200@gmail.com>
2025-11-08 19:46:04 +0100

    Compact method(writing bits instead of bools)

--- Aufgabe_2/Aufgabe_2.cpp
+++ Aufgabe_2/Aufgabe_2.cpp
@@ -4,46 +4,58 @@
 #include <stdio.h>
 #include <iostream>
 
-void Calculate(bool* array);
-int Analyse(bool* array);
+void Calculate(uint8_t* array, uint8_t size);
+int Analyse(uint8_t* array, uint8_t size);
 int main()
 {
     printf("Hello World!\n");
-
-    bool* array= (bool*)malloc(998);
+    uint8_t size = 1000 / 8;
+    uint8_t* array= (uint8_t*)malloc(size);
     if (!array) return -1;
-    memset(array,1,sizeof(array));
-    Calculate(array);
-    printf("%d,\n",Analyse(array));
+    memset(array,0xFF,size);
+    Calculate(array, size);
+    printf("%d,\n",Analyse(array, size));
 }
 
-void Calculate(bool* array)
+void Calculate(uint8_t* array, uint8_t size)
 {
-    for (int i = 0; i <= 998; i++)
+    for (uint8_t i = 0; i <= size; i++)
     {
-        if (!array[i]) continue;
-        int number = i + 2;
-        int p = number;
-        int numberToExclude = number * p;
-        while (numberToExclude < 1000)
+        for (int l = 0; l < 8; l++)
         {
-            array[numberToExclude - 2] = 0;
-            p++;
-            numberToExclude = number * p;
+            int number = i * 8 + l + 2;
+            int p = number;
+            int numberToExclude = number * p;
+            while (numberToExclude < size * 8)
+            {
+                uint8_t exclude_index = (numberToExclude - 2) / 8;
+                uint8_t array_value = array[exclude_index];
+                uint8_t mask = ~(1 << (7 - (numberToExclude - 2) % 8));
+                array_value &= mask;
+                array[exclude_index] = array_value;
+                p++;
+                numberToExclude = number * p;
+            }
         }
     }
 }
 
-int Analyse(bool * array)
+int Analyse(uint8_t* array, uint8_t size)
 {
     int result = 0;
-    for (int i = 0; i < 998; i++)
+    for (int i = 0; i < size; i++)
     {
-        if (array[i])
+        uint8_t array_value = array[i];
+        for (int l = 0; l < 8; l++)
         {
-            result++;
-            printf("%d,\n",i+2);
+            bool value = (array_value >> (7 - l)) & 1;
+            if (value)
+            {
+                result++;
+                printf("%d,\n", i * 8 + l + 2);
+            }
         }
+        
     }
     return result;
 }
